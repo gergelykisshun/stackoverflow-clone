bce8451727f8433ace9e90510a22dcc62af913ae
Gergely Kiss <gergely.kiss@r34dy.io>
2023-02-11 10:59:20 +0100

    next + mui + tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-xl text-blue-400">Hello Tailwind</h1>
+      <Button variant="outlined"> Hello mui </Button>
+    </>
+  );
+}
