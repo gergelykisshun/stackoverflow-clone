3ae7a17f4e2700e75bb98f1b0349948554cd090c
Gergely Kiss <gergely.kiss@r34dy.io>
2023-02-16 16:55:54 +0100

    responive tag cards on tag page

--- components/Cards/TagCard/TagCard.tsx
+++ components/Cards/TagCard/TagCard.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 const TagCard: FC<Props> = ({ tag }) => {
   return (
-    <Paper elevation={3} className="flex flex-col gap-3 items-center px-6 py-3">
-      <TagButton size="medium" tag={tag.name} />
+    <Paper className="flex flex-col gap-3 items-center px-6 py-3 xs:shadow-none sm:shadow-xl">
+      <TagButton size="xs:text-xs sm:text-base md:text-lg " tag={tag.name} />
       <Typography
         color="primary"
         className="xs:text-xs md:text-base w-full block truncate text-center"
