5a858730e6145ea69fbfbe3e928557530c452720
Gergely Kiss <gergely.kiss@r34dy.io>
2023-02-16 20:55:36 +0100

    when clicking tags update search field

--- components/TagButton/TagButton.tsx
+++ components/TagButton/TagButton.tsx
@@ -1,3 +1,4 @@
+import { useSearchStore } from "@/store/searchStore";
 import Link from "next/link";
 import { FC } from "react";
 
@@ -7,8 +8,11 @@ type Props = {
 };
 
 const TagButton: FC<Props> = ({ tag, size }) => {
+  const changeSearch = useSearchStore((state) => state.change);
+
   return (
     <Link
+      onClick={() => changeSearch(tag)}
       href={`/search?tagged=${tag}`}
       className={`py-1 px-2 font-light ${
         size || "text-xs"
