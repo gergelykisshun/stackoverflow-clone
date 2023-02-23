94e16b7b987b5003a84649b3f7b47b299838f685
Gergely Kiss <gergely.kiss@r34dy.io>
2023-02-23 08:37:04 +0100

    remove useless import

--- components/TagButton/TagButton.tsx
+++ components/TagButton/TagButton.tsx
@@ -1,7 +1,7 @@
 import { useSearchStore } from "@/store/searchStore";
 import { useThemeStore } from "@/store/themeStore";
 import styled from "@emotion/styled";
-import { Box, Button, ButtonProps, ToggleButton } from "@mui/material";
+import { Box, Button, ButtonProps } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import Link from "next/link";
 import { FC } from "react";
